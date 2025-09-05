92799f47029922714d455b7670f3f965dbfdb1a2
Kristan <itan_07@hotmail.com>
2025-09-05 14:33:55 -0700

    more tests

--- src/core/bot/bot.test.ts
+++ src/core/bot/bot.test.ts
@@ -1,5 +1,8 @@
 import { expect, spyOn, test } from "bun:test";
-import { GroupTxtMsg } from "../../helpers/Whatsapp.helper.mocks";
+import { GroupMsg_CHATID, GroupMsg_SENDERID, GroupTxtMsg, IndividualMsg_CHATID, IndividualTxtMsg } from "../../helpers/Whatsapp.helper.mocks";
+import { MsgType, SenderType } from "../../Msg.types";
+import { WhatsSocket_Submodule_Receiver } from "../whats_socket/internals/WhatsSocket.receiver";
+import { WhatsSocket_Submodule_SugarSender } from "../whats_socket/internals/WhatsSocket.sugarsenders";
 import WhatsSocketMock from "../whats_socket/mocks/WhatsSocket.mock";
 import Bot from "./bot";
 import type { ChatContext } from "./internals/ChatContext";
@@ -17,10 +20,13 @@ import type { ICommand, RawMsgAPI } from "./internals/IBotCommand";
  *    3.2. [X] What happens if 2 commands have same alias? (should throw error) (not if differnet type)
  * 
  * 6. [] Can run commands and tags
- *      [] Can differentiate tags and commands (if having a tag with same name like a command)
- *      [] Can run a command with their ALIAS ONLY
- *      [] Commands receives correct arguments from the bot
+ *      [X] Can run a normal command
+ *      [X] CAn run a tag command
+ *      [X] Can differentiate tags and commands (if having a tag with same name like a command)
+ *      [X] Can run a command with their ALIAS ONLY
+ *      [X] Commands receives correct arguments from the bot
  *          [] For incoming quoted msg, should send argument successfully to command
+ *      [] When running only @ or ! (common prefixes) shouldn't do anything
  * 7. [] Unexpected exception: Should not break bot and console.log error instead serialized as json text
  * 8. [] Expected Error "Command rejection": Should not break and console log like info
  * 9. [] Expected Error "command not rejection" (by timeout): Should console.log gracefully
@@ -69,7 +75,7 @@ class CommandDesnormalized implements ICommand {
 
 const generateToolkit = () => {
   const mockSocket = new WhatsSocketMock({ minimumMilisecondsDelayBetweenMsgs: 1, maxQueueLimit: 10 });
-  const bot = new Bot({ ownWhatsSocketImplementation: mockSocket, commandPrefix: "!", loggerMode: "recommended" });
+  const bot = new Bot({ ownWhatsSocketImplementation: mockSocket, commandPrefix: "!", loggerMode: "recommended", enableCommandSafeNet: false });
   return { socket: mockSocket, bot };
 };
 const idealCommand = new CommandIdeal();
@@ -105,28 +111,210 @@ test("Commands_WhenAddingNewCommands_ShouldAddThemToInternalCommandSubmodule", (
   expect(bot.Commands.TagCommands).toHaveLength(1);
 });
 
-test("Running_WhenRunningSimple_NORMALCOMMAND_ShouldSuccessfully", async () => {
+test("Running_WhenRunningSimple_NORMALCOMMAND_FROMGROUP_ShouldSuccessfully", async () => {
   const { bot, socket } = generateToolkit();
   const command: ICommand = {
     name: "hello",
     description: "mock hello description",
     aliases: [],
-    async run(_chat, _api, _args) {
-      throw new Error("bruh");
-      console.log(" ==== I'm code running inside a command! ====");
+    async run(chat, api, args) {
+      expect(chat).toBeDefined();
+
+      expect(api).toBeDefined();
+      expect(api.Receive).toBeInstanceOf(WhatsSocket_Submodule_Receiver);
+      expect(api.Send).toBeInstanceOf(WhatsSocket_Submodule_SugarSender);
+
+      expect(args).toBeDefined();
+      expect(args.args).toEqual(["arg1", "arg2", "arg3", "arg4", "..."]);
+      expect(args.chatId).toBe(GroupMsg_CHATID);
+      expect(args.msgType).toBe(MsgType.Text);
+      expect(args.originalRawMsg).toBe(GroupTxtMsg);
+      expect(args.quotedMsg).toBe(null);
+      expect(args.senderType).toBe(SenderType.Group);
+      expect(args.userId).toBe(GroupMsg_SENDERID);
+
+      console.log(" ==== I'm code running inside a normal command! FROM GROUP ====");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Normal);
+  bot.Start();
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "!hello arg1 arg2 arg3 arg4 ..." });
+  expect(commandRunSpy).toHaveBeenCalledTimes(1);
+});
+
+test("Running_WhenRunningSimple_TAGCOMMAND_FROMGROUP_ShouldBeSuccessfully", async () => {
+  const { bot, socket } = generateToolkit();
+  const command: ICommand = {
+    name: "hellotag",
+    description: "mock hello tag description",
+    aliases: [],
+    async run(chat, api, args) {
+      expect(chat).toBeDefined();
+
+      expect(api).toBeDefined();
+      expect(api.Receive).toBeInstanceOf(WhatsSocket_Submodule_Receiver);
+      expect(api.Send).toBeInstanceOf(WhatsSocket_Submodule_SugarSender);
+
+      expect(args).toBeDefined();
+      expect(args.args).toEqual(["arg1", "arg2", "arg3", "arg4", "..."]);
+      expect(args.chatId).toBe(GroupMsg_CHATID);
+      expect(args.msgType).toBe(MsgType.Text);
+      expect(args.originalRawMsg).toBe(GroupTxtMsg);
+      expect(args.quotedMsg).toBe(null);
+      expect(args.senderType).toBe(SenderType.Group);
+      expect(args.userId).toBe(GroupMsg_SENDERID);
+
+      console.log(" ==== I'm code running inside a tag command! FROM GROUP ====");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Tag);
+  bot.Start();
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "@hellotag arg1 arg2 arg3 arg4 ..." });
+  expect(commandRunSpy).toHaveBeenCalledTimes(1);
+});
+
+test("Running_WhenRunningSimple_NORMALCOMMAND_FROMINDIVIDUAL_ShouldSuccessfully", async () => {
+  const { bot, socket } = generateToolkit();
+  const command: ICommand = {
+    name: "hello",
+    description: "mock hello description",
+    aliases: [],
+    async run(chat, api, args) {
+      expect(chat).toBeDefined();
+
+      expect(api).toBeDefined();
+      expect(api.Receive).toBeInstanceOf(WhatsSocket_Submodule_Receiver);
+      expect(api.Send).toBeInstanceOf(WhatsSocket_Submodule_SugarSender);
+
+      expect(args).toBeDefined();
+      expect(args.args).toEqual(["arg1", "arg2", "arg3", "arg4", "..."]);
+      expect(args.chatId).toBe(IndividualMsg_CHATID);
+      expect(args.msgType).toBe(MsgType.Text);
+      expect(args.originalRawMsg).toBe(IndividualTxtMsg);
+      expect(args.quotedMsg).toBe(null);
+
+      //Comes from private msg
+      expect(args.senderType).toBe(SenderType.Individual);
+      expect(args.userId).toBe(null);
+
+      console.log(" ==== I'm code running inside a normal command! FROM INDIVIDUAL ====");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Normal);
+  bot.Start();
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(IndividualTxtMsg, { replaceTextWith: "!hello arg1 arg2 arg3 arg4 ..." });
+  expect(commandRunSpy).toHaveBeenCalledTimes(1);
+});
+
+test("Running_WhenRunningSimple_TAGCOMMAND_FROMINDIVIDUAL_ShouldBeSuccessfully", async () => {
+  const { bot, socket } = generateToolkit();
+  const command: ICommand = {
+    name: "hellotag",
+    description: "mock hello tag description",
+    aliases: [],
+    async run(chat, api, args) {
+      expect(chat).toBeDefined();
+
+      expect(api).toBeDefined();
+      expect(api.Receive).toBeInstanceOf(WhatsSocket_Submodule_Receiver);
+      expect(api.Send).toBeInstanceOf(WhatsSocket_Submodule_SugarSender);
+
+      expect(args).toBeDefined();
+      expect(args.args).toEqual(["arg1", "arg2", "arg3", "arg4", "..."]);
+      expect(args.chatId).toBe(IndividualMsg_CHATID);
+      expect(args.msgType).toBe(MsgType.Text);
+      expect(args.originalRawMsg).toBe(IndividualTxtMsg);
+      expect(args.quotedMsg).toBe(null);
+
+      //Comes from private msg
+      expect(args.senderType).toBe(SenderType.Individual);
+      expect(args.userId).toBe(null);
+
+      console.log(" ==== I'm code running inside a tag command! FROM INDIVIDUAL ====");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Tag);
+  bot.Start();
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(IndividualTxtMsg, { replaceTextWith: "@hellotag arg1 arg2 arg3 arg4 ..." });
+  expect(commandRunSpy).toHaveBeenCalledTimes(1);
+});
+
+test("Running_MsgTagWithCommandNormal_ShouldNotReceiveIt", async () => {
+  const { bot, socket } = generateToolkit();
+  const command: ICommand = {
+    name: "mycommand_normal",
+    description: "mock mycommand_normal description",
+    async run(_ctx, _rawMsgApi, _args) {
+      throw new Error("This command MUST NOT be executed!");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Normal);
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "@mycommand_normal hello world" });
+  expect(commandRunSpy).toHaveBeenCalledTimes(0);
+});
+
+test("Running_MsgNormalCommandWithTag_ShouldNotReceiveIt", async () => {
+  const { bot, socket } = generateToolkit();
+  const command: ICommand = {
+    name: "mycommand_tag",
+    description: "mock mycommand_tag description",
+    async run(_ctx, _rawMsgApi, _args) {
+      throw new Error("This command MUST NOT be executed!");
+    },
+  };
+  bot.Commands.Add(command, CommandType.Tag);
+  const commandRunSpy = spyOn(command, "run");
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "!mycommand_normal hello world" });
+  expect(commandRunSpy).toHaveBeenCalledTimes(0);
+});
+
+test("Running_WhenHavingTwoDifferentTypeCommandsWithSameName_ShouldRecognizeThemAsDifferent", async () => {
+  const { bot, socket } = generateToolkit();
+
+  let normalCommandCalled: boolean = false;
+  const commandNormal: ICommand = {
+    name: "samecommand",
+    description: "mock samecomand description",
+    async run(_ctx, _rawMsgApi, _args) {
+      normalCommandCalled = true;
+    },
+  };
+  let tagCommandCalled: boolean = false;
+  const commandTag: ICommand = {
+    name: "samecommand",
+    description: "mock samecomand description",
+    async run(_ctx, _rawMsgApi, _args) {
+      tagCommandCalled = true;
     },
   };
 
-  const commandRunSpy = spyOn(command, "run");
-  bot.Commands.Add(command, CommandType.Normal);
-  bot.Start();
-  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "!hello firstarg secondarg" });
+  bot.Commands.Add(commandNormal, CommandType.Normal);
+  bot.Commands.Add(commandTag, CommandType.Tag);
+  const normalCommandRunSpy = spyOn(commandNormal, "run");
+  const tagCommandRunSpy = spyOn(commandTag, "run");
 
-  expect(commandRunSpy).toHaveBeenCalledTimes(1);
-  const [chat, api, args] = commandRunSpy.mock.lastCall!;
-  expect(chat).not.toBeNull();
-  expect(api).not.toBeNull();
-  expect(args).not.toBeNull();
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "!samecommand" });
+
+  expect(normalCommandRunSpy).toHaveBeenCalledTimes(1);
+  expect(normalCommandCalled).toBe(true);
+
+  expect(tagCommandRunSpy).toHaveBeenCalledTimes(0);
+  expect(tagCommandCalled).toBe(false);
+
+  await socket.MockSendMsgAsync(GroupTxtMsg, { replaceTextWith: "@samecommand" });
+
+  expect(normalCommandRunSpy).toHaveBeenCalledTimes(1);
+  expect(normalCommandCalled).toBe(true);
+
+  expect(tagCommandRunSpy).toHaveBeenCalledTimes(1);
+  expect(tagCommandCalled).toBe(true);
 });
+
+// test("");
 
 //TODO: Verify command names can't have " " spaces, only 1 word long
