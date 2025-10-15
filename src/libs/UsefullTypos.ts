c8b3856a8cc9696a01e8515d21d8b17ffc7f5261
Kristan Ruíz <itan_07@hotmail.com>
2025-10-15 04:24:20 -0700

    types: Adding DeepRequired<T>

--- src/libs/UsefullTypos.ts
+++ src/libs/UsefullTypos.ts
@@ -3,3 +3,22 @@ export type DeepPartial<T> = T extends object
       [P in keyof T]?: DeepPartial<T[P]>;
     }
   : T;
+
+/**
+ * Recursively makes all properties of an object required.
+ */
+export type DeepRequired<T> = T extends (...args: any[]) => any
+  ? T
+  : T extends any[]
+  ? DeepRequiredArray<T[number]>
+  : T extends object
+  ? DeepRequiredObject<T>
+  : T;
+
+/** @private */
+interface DeepRequiredArray<T> extends Array<DeepRequired<T>> {}
+
+/** @private */
+type DeepRequiredObject<T> = {
+  [P in keyof T]-?: DeepRequired<T[P]>;
+};
