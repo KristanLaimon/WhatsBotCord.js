d3c76332437b6aa1029b72d8a20f6b09b8293c95
Kristan Ruíz <itan_07@hotmail.com>
2025-10-24 09:49:58 -0700

    chroe: Deleting innecesary index.main.test.ts file

--- src/index.main.test.ts
+++ src/index.main.test.ts
@@ -1,17 +1,28 @@
-import { expect, test } from "bun:test";
+import { test } from "bun:test";
 import type { AdditionalAPI, CommandArgs, IChatContext } from "./index.js";
-import { type ICommand, ChatMock } from "./index.js";
+import { type ICommand, ChatMock, SenderType } from "./index.js";
 
-test("Testing testing", async () => {
+test("Issue before update testt", async () => {
   class Com implements ICommand {
     name: string = "com";
-    public async run(ctx: IChatContext, _api: AdditionalAPI, _args: CommandArgs): Promise<void> {
-      await ctx.SendContact({ name: "chris", phone: "7132322" });
+    public async run(_ctx: IChatContext, _api: AdditionalAPI, args: CommandArgs): Promise<void> {
+      console.log(args.participantIdLID); //It could be string | null
+      console.log(args.participantIdPN); //It could be string | null
     }
   }
+  const chat = new ChatMock(new Com(), {
+    // participantId_LID: undefined, //this sets args.participantLID to "fakeParticipantID@lid"
+    // participantId_PN: undefined, // this sets args.participantPN to  "fakeParticipantID@s.whatsapp.net"
+    senderType: SenderType.Group,
+  });
+  const chat2 = new ChatMock(new Com(), {
+    participantId_LID: "", //this sets args.participantLID to "fakeParticipantID@lid"
+    participantId_PN: "", // this sets args.participantPN to  "fakeParticipantID@s.whatsapp.net"
+    senderType: SenderType.Group,
+  });
 
-  const chat = new ChatMock(new Com());
+  //So... how can I set participantId_LID to null? when simulating it comes from individual chat
+  //or error with whatsapp servers when fetching that arg.variable?
   await chat.StartChatSimulation();
-  expect(chat.SentFromCommand.Contacts).toHaveLength(1);
-  console.log(chat.SentFromCommand.Contacts);
+  await chat2.StartChatSimulation();
 });
